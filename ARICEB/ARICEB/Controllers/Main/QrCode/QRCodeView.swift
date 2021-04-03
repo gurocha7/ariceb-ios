b9ce401a59410ba55785e70e1daf2137fce7b444
Corevent Mobile <contato.corevent@gmail.com>
2021-04-02 22:12:14 -0300

    new icon image for qrcode

--- ARICEB/ARICEB/Controllers/Main/QrCode/QRCodeView.swift
+++ ARICEB/ARICEB/Controllers/Main/QrCode/QRCodeView.swift
@@ -9,9 +9,21 @@ import UIKit
 import Reusable
 
 class QRCodeView: UIView, NibLoadable{
-
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var imageQRCode: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupLayout()
+    }
+    
+    private func setupLayout(){
+        contentView.backgroundColor = mainColor
+        contentView.applyCorner(corner: 15)
+        contentView.applyBorder(borderWidth: 0.5, borderColor: UIColor.gray.cgColor)
+        imageQRCode.image = UIImage(named: "icon-qrcode")?.withRenderingMode(.alwaysOriginal)
+        labelTitle.text = "QRCODE\nEntenda como funciona a localização via QrCode."
     }
     
 }
