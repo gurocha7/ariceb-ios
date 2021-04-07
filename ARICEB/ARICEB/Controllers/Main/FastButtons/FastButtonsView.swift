cbf8f24c293ae7c29e046e6b1347d83c71f1eef1
Corevent Mobile <contato.corevent@gmail.com>
2021-04-06 22:34:10 -0300

    setup layout for fastButtonsView

--- ARICEB/ARICEB/Controllers/Main/FastButtons/FastButtonsView.swift
+++ ARICEB/ARICEB/Controllers/Main/FastButtons/FastButtonsView.swift
@@ -9,9 +9,31 @@ import UIKit
 import Reusable
 
 class FastButtonsView: UIView, NibLoadable {
-
+    
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var viewLeft: UIView!
+    @IBOutlet weak var viewRight: UIView!
+    
+    @IBAction func buttonLeftAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonRightAction(_ sender: Any) {
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupLayout()
+    }
+    
+    private func setupLayout(){
+        viewLeft.applyCorner(corner: 15)
+        viewLeft.applyBorder(borderWidth: 0.5, borderColor: UIColor.gray.cgColor)
+        viewRight.applyCorner(corner: 15)
+        viewRight.applyBorder(borderWidth: 0.5, borderColor: UIColor.gray.cgColor)
     }
 
 }
