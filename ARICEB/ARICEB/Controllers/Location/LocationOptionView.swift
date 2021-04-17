d9fd30a7488660596ff99f6e1d216db1683b1c17
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-04-17 11:59:43 -0300

    LocationOptionView created

---
+++ ARICEB/ARICEB/Controllers/Location/LocationOptionView.swift
@@ -0,0 +1,17 @@
+//
+//  LocationOptionView.swift
+//  ARICEB
+//
+//  Created by Gustavo Rocha on 17/04/21.
+//
+
+import UIKit
+import Reusable
+
+class LocationOptionView: UIView, NibLoadable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
