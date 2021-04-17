22b8d958a826414a332fe8d5feedbc38cee040c2
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-04-17 16:07:28 -0300

    customize tableView for menu

--- ARICEB/ARICEB/Coordinator/Menu/MenuView.swift
+++ ARICEB/ARICEB/Coordinator/Menu/MenuView.swift
@@ -9,9 +9,28 @@ import UIKit
 import Reusable
 
 class MenuView: UIView, NibLoadable {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setup()
+    }
+    
+    private func setup(){
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+}
+
+extension MenuView: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "row \(indexPath.row)"
+        return cell
+    }
 }
