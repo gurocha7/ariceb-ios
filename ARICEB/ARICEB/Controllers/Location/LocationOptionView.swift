905416a4e3fd46c9b0ebe29b29d1f8a9252ce2e9
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-04-17 12:22:22 -0300

    stackView and your subviews setup

--- ARICEB/ARICEB/Controllers/Location/LocationOptionView.swift
+++ ARICEB/ARICEB/Controllers/Location/LocationOptionView.swift
@@ -9,9 +9,22 @@ import UIKit
 import Reusable
 
 class LocationOptionView: UIView, NibLoadable {
-
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func buttonAction(_ sender: Any) {
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    func setup(){
+        contentView.applyCorner(corner: 10)
+        contentView.applyBorder(borderWidth: 1, borderColor: UIColor.darkGray.cgColor)
     }
 
 }
--- ARICEB/ARICEB/Controllers/Location/LocationView.swift
+++ ARICEB/ARICEB/Controllers/Location/LocationView.swift
@@ -10,8 +10,40 @@ import Reusable
 
 class LocationView: UIView, NibLoadable {
     @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var stack: UIStackView!
+    @IBOutlet weak var gpsView: LocationOptionView!
+    @IBOutlet weak var qrCodeView: LocationOptionView!
+    @IBOutlet weak var manualView: LocationOptionView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setup()
+    }
+    
+    private func setup(){
+        clearStack()
+        labelTitle.text = "Você pode determinar seu ponto de origem utilizando  um dos métodos a seguir:"
+        setupStack()
+    }
+    
+    
+    private func setupStack(){
+        gpsView = LocationOptionView.loadFromNib()
+        gpsView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        qrCodeView = LocationOptionView.loadFromNib()
+        qrCodeView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        manualView = LocationOptionView.loadFromNib()
+        manualView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        stack.addArrangedSubview(gpsView)
+        stack.addArrangedSubview(qrCodeView)
+        stack.addArrangedSubview(manualView)
+        gpsView.setup()
+        qrCodeView.setup()
+        manualView.setup()
+    }
+    
+    private func clearStack(){
+        stack.arrangedSubviews.forEach { $0.removeFromSuperview() }
     }
 }
