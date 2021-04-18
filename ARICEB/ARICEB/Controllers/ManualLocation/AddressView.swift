5af423edd6280d562ef66347a052f1a403b1d184
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-04-17 22:00:32 -0300

    setup stack arranged and subviews

---
+++ ARICEB/ARICEB/Controllers/ManualLocation/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  ARICEB
+//
+//  Created by Gustavo Rocha on 17/04/21.
+//
+
+import UIKit
+import Reusable
+
+class AddressView: UIView, NibLoadable {
+    @IBOutlet weak var contentView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- ARICEB/ARICEB/Controllers/ManualLocation/ManualLocationView.swift
+++ ARICEB/ARICEB/Controllers/ManualLocation/ManualLocationView.swift
@@ -11,13 +11,41 @@ import Reusable
 class ManualLocationView: UIView, NibLoadable {
     
     @IBOutlet weak var infoView: UIView!
+    @IBOutlet weak var stack: UIStackView!
     @IBOutlet weak var buttonConfirm: UIButton!
-    @IBOutlet weak var firstAddress: UIView!
-    @IBOutlet weak var secondAddress: UIView!
-    @IBOutlet weak var thirdAddress: UIView!
+    @IBOutlet weak var firstAddressView: AddressView!
+    @IBOutlet weak var secondAddressView: AddressView!
+    @IBOutlet weak var thirdAddressView: AddressView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setup()
+    }
+    
+    private func setup(){
+        clearStack()
+        setupStack()
+        setupButton()
     }
 
+    private func clearStack(){
+        stack.arrangedSubviews.forEach { $0.removeFromSuperview() }
+    }
+    
+    private func setupStack(){
+        firstAddressView = AddressView.loadFromNib()
+        secondAddressView = AddressView.loadFromNib()
+        thirdAddressView = AddressView.loadFromNib()
+        firstAddressView.heightAnchor.constraint(equalToConstant: 42).isActive = true
+        secondAddressView.heightAnchor.constraint(equalToConstant: 42).isActive = true
+        thirdAddressView.heightAnchor.constraint(equalToConstant: 42).isActive = true
+        stack.addArrangedSubview(firstAddressView)
+        stack.addArrangedSubview(secondAddressView)
+        stack.addArrangedSubview(thirdAddressView)
+    }
+    
+    private func setupButton(){
+        
+    }
+    
 }
