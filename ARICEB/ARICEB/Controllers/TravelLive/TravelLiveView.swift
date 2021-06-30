3c1032d4f5b8a50ca81db22bd11cb3156f5df0ed
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-06-29 21:02:07 -0300

    🧩: load track movements and orientations with AR

--- ARICEB/ARICEB/Controllers/TravelLive/TravelLiveView.swift
+++ ARICEB/ARICEB/Controllers/TravelLive/TravelLiveView.swift
@@ -7,16 +7,21 @@
 
 import UIKit
 import Reusable
-import SceneKit
 import ARKit
 
 class TravelLiveView: UIView, NibLoadable {
+    //MARK: - Propeties
+    private let arConfig = ARWorldTrackingConfiguration() //faz a utilização da camêra com AR e monitora a posicão e orientação do device
     
+    //MARK: - OUTLETS
     @IBOutlet weak var sceneView: ARSCNView!
-    
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupUI()
     }
 
+    private func setupUI() {
+        sceneView.session.run(arConfig)
+    }
 }
