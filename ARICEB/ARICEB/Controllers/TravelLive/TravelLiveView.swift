fcf40d4cef1f7f433849d01b6001c5dfb4179196
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-06-30 23:09:20 -0300

    🧩: added triangle extension

--- ARICEB/ARICEB/Controllers/TravelLive/TravelLiveView.swift
+++ ARICEB/ARICEB/Controllers/TravelLive/TravelLiveView.swift
@@ -34,10 +34,41 @@ class TravelLiveView: UIView, NibLoadable {
         box.firstMaterial?.diffuse.contents = UIColor.purple //adiciona cor para o material desenhado na tela
         box.firstMaterial?.specular.contents = 0.7 //adiciona brilho ao material
         
-        let nodeBox = SCNNode(geometry: box)
+        let triangle = SCNGeometry.triangleFrom(vector1: SCNVector3(1, 0, 1), vector2: SCNVector3(1, 1, -1), vector3: SCNVector3(-1, 1, 1))
+        
+        let nodeBox = SCNNode(geometry: triangle)
         nodeBox.position = SCNVector3(0, 0, -0.2)
         nodeBox.scale = SCNVector3(0.1, 0.1, 0.1)
+        
         firstScene.rootNode.addChildNode(nodeBox)
         sceneView.scene = firstScene
+    }
+}
+
+extension SCNGeometry {
+    static func Plane(width: CGFloat, height: CGFloat) -> SCNGeometry {
+        let src = SCNGeometrySource(vertices: [
+            SCNVector3(-width / 2, -height / 2, 0),
+            SCNVector3(width / 2, -height / 2, 0),
+            SCNVector3(-width / 2, height / 2, 0),
+            SCNVector3(width / 2, height / 2, 0),
+        ])
+        let indices: [UInt32] = [0, 1, 2, 3]
+        let normals = SCNGeometrySource(normals: [SCNVector3](repeating: SCNVector3(0, 0, 1), count: 4) )
+        let textureMap = SCNGeometrySource(textureCoordinates: [
+            CGPoint(x: 0, y: 1),
+            CGPoint(x: 1, y: 1),
+            CGPoint(x: 0, y: 0),
+            CGPoint(x: 1, y: 0)
+        ])
+        let inds = SCNGeometryElement(indices: indices, primitiveType: .triangleStrip)
+        return SCNGeometry.init(sources: [src, normals, textureMap], elements: [inds])
+    }
+    
+    class func triangleFrom(vector1: SCNVector3, vector2: SCNVector3, vector3: SCNVector3) -> SCNGeometry {
+        let indices: [Int32] = [0, 1, 2]
+        let source = SCNGeometrySource(vertices: [vector1, vector2, vector3])
+        let element = SCNGeometryElement(indices: indices, primitiveType: .triangles)
+        return SCNGeometry(sources: [source], elements: [element])
     }
 }
