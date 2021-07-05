fd3e76ca9692d0096ff69afaccc89e174ac8250d
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-07-04 22:28:36 -0300

    🧩: show route for destiny

--- ARICEB/ARICEB/Controllers/TravelLive/TravelLiveViewController.swift
+++ ARICEB/ARICEB/Controllers/TravelLive/TravelLiveViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import ARCL
+import ARKit
+import MapKit
 
 class TravelLiveViewController: BaseViewController {
     
@@ -19,6 +21,7 @@ class TravelLiveViewController: BaseViewController {
     override func loadView() {
         super.loadView()
         view = customView
+        setupUI()
     }
     
     override func viewDidLoad() {
@@ -27,14 +30,72 @@ class TravelLiveViewController: BaseViewController {
         bindEvents()
         getMockLocation()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneLocationView.pause()
+    }
 
+    private func setupUI() {
+        sceneLocationView.showAxesNode = true
+        sceneLocationView.showFeaturePoints = false
+        sceneLocationView.locationNodeTouchDelegate = self
+//        sceneLocationView.arViewDelegate  = self
+        customView.addSubview(sceneLocationView)
+        sceneLocationView.frame = customView.bounds
+    }
+    
     private func bindEvents() {
         viewModel.didShowRoutes = { [weak self] (routes) in
-            print("rotas encontradas: ", routes)
+            self?.sceneLocationView.run()
+            self?.addRoutesToScene(routes: routes)
         }
     }
     
     private func getMockLocation() {
-        viewModel.getRoute(originLat: -20.398255, originLong: -43.506740, destinyLat: -20.395751, destinyLong: -43.509350)
+        viewModel.getRoute(originLat: -20.153360, originLong: -44.903800, destinyLat: -20.153983, destinyLong: -44.903949)
+    }
+    
+    private func addRoutesToScene(routes: [MKRoute]? ) {
+        guard sceneLocationView.sceneLocationManager.currentLocation != nil else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+                self?.addRoutesToScene(routes: routes)
+            }
+            return
+        }
+        
+        if let routesValue = routes {
+            print("ADDED ROUTES")
+            sceneLocationView.addRoutes(routes: routesValue) { distance -> SCNBox in
+                print("SETANDO O BOX")
+                let box = SCNBox(width: 1.75, height: 0.5, length: distance, chamferRadius: 0.25)
+                box.firstMaterial?.diffuse.contents = UIColor.yellow
+                return box
+            }
+        }else{
+            print("NAO CONSEGUIU ADICIONAR AS ROTAS")
+            sceneLocationView.pause()
+        }
+        sceneLocationView.autoenablesDefaultLighting = true
+    }
+}
+
+extension TravelLiveViewController: LNTouchDelegate {
+    func annotationNodeTouched(node: AnnotationNode) {
+        if let node = node.parent as? LocationNode {
+            let coords = "\(node.location.coordinate.latitude.short)° \(node.location.coordinate.longitude.short)°"
+            let altitude = "\(node.location.altitude.short)m"
+            let tag = node.tag ?? ""
+//            nodePositionLabel.text = " Annotation node at \(coords), \(altitude) - \(tag)"
+            print("###Annotation node at \(coords), \(altitude) - \(tag)")
+        }
+    }
+    
+    func locationNodeTouched(node: LocationNode) {
+        let coords = "\(node.location.coordinate.latitude.short)° \(node.location.coordinate.longitude.short)°"
+        let altitude = "\(node.location.altitude.short)m"
+        let tag = node.tag ?? ""
+//        nodePositionLabel.text = " Location node at \(coords), \(altitude) - \(tag)"
+        print("###Location node at \(coords), \(altitude) - \(tag)")
     }
 }
