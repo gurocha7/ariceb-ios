09537287328493b1fd73925ed022f2ee32467d97
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-08-17 01:07:05 -0300

    🧩: new controllers

---
+++ ARICEB/ARICEB/Coordinator/Submenu/FAQDetails/FAQDetails4ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FAQDetails4ViewController.swift
+//  ARICEB
+//
+//  Created by Gustavo Rocha on 17/08/21.
+//
+
+import UIKit
+
+class FAQDetails4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Como escanear um qrcode?"
+    }
+
+}
---
+++ ARICEB/ARICEB/Coordinator/Submenu/FAQDetails/FAQDetails5ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FAQDetails5ViewController.swift
+//  ARICEB
+//
+//  Created by Gustavo Rocha on 17/08/21.
+//
+
+import UIKit
+
+class FAQDetails5ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Interpretando as setas"
+    }
+
+}
